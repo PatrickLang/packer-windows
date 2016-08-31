f9fb76de70a612fe19a235d7c05bb4e7d67c24b1
Patrick Lang <patrick.lang@hotmail.com>
2016-08-30 17:46:53 -0700

    Adding scripts, partially duplicated with JSON right now

---
+++ scripts/dc/create-domain.ps1
@@ -0,0 +1,8 @@
+param(
+    $DomainName,
+    $DomainNetbiosName
+)
+
+
+Import-Module ADDSDeployment
+Install-ADDSForest -DomainName $DomainName -DomainNetbiosName $DomainNetbiosName -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -ForestMode "Win2012R2" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
