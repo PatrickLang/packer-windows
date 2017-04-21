2eea3f2f2778a65bb0dc90d012b1daacb841c26f
Patrick Lang <patrick.lang@hotmail.com>
2017-04-20 18:56:34 -0700

    moving out of c:\updates folder

--- scripts/install-offline-updates.ps1
+++ scripts/install-offline-updates.ps1
@@ -1,24 +1,22 @@
-mkdir c:\updates
-cd c:\updates
 $httpRoot = "http://$($ENV:PACKER_HTTP_ADDR)"
 Write-Output "Searching for updates at $httpRoot"
 $httpIndex = Invoke-WebRequest -UseBasicParsing $httpRoot
 $httpIndex.Links | Select-Object -Property href | ForEach-Object {
     if ($_.href -Like "*msu") {
         $fullUrl = "$httpRoot/$($_.href)"
-        Write-Output "Downloading $fullUrl"
-        Invoke-WebRequest -UseBasicParsing $fullUrl -OutFile $($_.href)
+        $localPath = "$($ENV:TEMP)\\$($_.href)"
+        Write-Output "Downloading $fullUrl to $localPath"
+        Invoke-WebRequest -UseBasicParsing $fullUrl -OutFile $localPath
         #dism.exe /online /add-package:"$($_.href)"
         Write-Output "Starting update..."
-        wusa.exe "$PWD\\$($_.href)" /quiet /norestart
+        wusa.exe $localPath /quiet /norestart /log:"$($ENV:TEMP)\\$($_.href).log"
         $t = Measure-Command { Get-Process wusa | Wait-Process }
         Write-Output "Update process finished in $($t.ToString())"
+        notepad.exe # TEMP for debugging
+        Remove-Item $localPath
     }
     else
     {
         Write-Output "Skipping $($_.href)" 
     }
 }
-notepad.exe # TEMP
-cd ~
-Remove-Item -Recurse -Force c:\updates
