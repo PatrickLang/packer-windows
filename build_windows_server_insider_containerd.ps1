4f90c640ab4d835d68906ba999d3144c8a363100
Patrick Lang <plang@microsoft.com>
2018-09-12 11:37:15 -0700

    Updating to new insider build

--- build_windows_server_insider_containerd.ps1
+++ build_windows_server_insider_containerd.ps1
@@ -1,3 +1,4 @@
 packer build --only=hyperv-iso `
-             --var iso_url=./iso/17755.iso `
+             --var iso_url=./iso/17758.iso `
+             --var iso_checksum='D999CCCB74FF2EBBFFAA2B6A43C3CAE8CA24875426119CC5FDAC7864542E57A2' `
              windows_server_insider_containerd.json
